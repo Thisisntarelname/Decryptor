079557ce7257d3f10bdd307d0ebbe4dc69c8e3d8
Fake Name <80899773+Thisisntarelname@users.noreply.github.com>
2022-03-31 21:32:08 +0000

    Star

--- main.py
+++ main.py
@@ -17,11 +17,86 @@ def animate():
 t = threading.Thread(target=animate)
 t.start()
 
+def clear():
+  os.system('cls' if os.name == 'nt' else 'clear')
 i=0
-while i<10000000:
-  i=i+1
 
 done = True
-os.system('cls' if os.name == 'nt' else 'clear')
+clear()
 
-print("done")
+def ceaserCipher(key):
+  print('Ceaser cipher')
+def ceasercipherUnknown():
+  print("Ceaser Cipher unknown")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+choice = input("Do you want to decode:\n1. Cipher Unknown\n2. Ceaser cipher\n3. Substitution cipher\n4. Atbash\n5. Bacon cipher\n6. Reverse\n7. NATO Phonetic Alphabet\n8. Affine cipher\n9. Vigenere cipher\n10. Rail Fence cipher\n")
+
+if choice=="1":
+  print("Code Unknown")
+elif choice=="2":
+  print("Ceaser Cipher")
+  choice = input("Do you know the key? (y\n) ")
+  if choice =="y":
+    key = input("Enter key: ")
+    ceaserCipher(key)
+  else:
+    ceasercipherUnkown
+  
+elif choice=="3":
+  print("Substitution cipher")
+  choice = input("Do you know the key? (y\n) ")
+
+elif choice=="4":
+  print("Atbash")
+  choice = input("Do you know the key? (y\n) ")
+
+elif choice=="5":
+  print("Bacon cipher")
+elif choice=="6":
+  print("Reverse")
+elif choice=="7":
+  print("NATO Phonetic Alphabet")
+elif choice=="8":
+  print("Affine Cipher")
+elif choice=="9":
+  print("Vigenere cipher")
+elif choice=="10":
+  print("Rail Fence cipher")
